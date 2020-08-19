3dc152f85e3304bf40c8e8de990ec193066dc7e1
Nazmul@AnDIT <webguru071@gmail.com>
2020-08-19 12:52:01 +0600

    3rd commit

--- TutorialFirst/urls.py
+++ TutorialFirst/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('calc.urls')),
+    path('', include('trial_app.urls')),
     path('admin/', admin.site.urls),
     path("travello/", include('travello.urls')),
 ]
